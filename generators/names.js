b8a83d3728f578723b15a25829403e9b55558c6a
rnice4christ <rnice4christ@gmail.com>
2017-06-10 18:52:55 -0500

    Initial commit

---
+++ generators/names.js
@@ -0,0 +1,11 @@
+let wookie_names = require('../data/names/wookie_names.json');
+let getRandomInt = require('../lib/generator_helper').getRandomInt;
+module.exports = {
+	
+	/* Wookie names are made up from a list of prefixes and suffixes, i.e. Chew and bacca for Chewbacca.*/
+	Wookie() {
+		var prefixes = wookie_names.prefixes;
+  		var suffixes = wookie_names.suffixes;
+  		return prefixes[getRandomInt(prefixes.length)] + suffixes[getRandomInt(suffixes.length)];
+  	}
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+const name_generator = require('./generators/names');
+
+exports.names = {
+	Wookie : name_generator.Wookie
+}
